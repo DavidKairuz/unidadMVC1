eafb6dcc2b5eb749b559783ab52dee6df3ae85f3
DEK <davide.kairuz@gmail.com>
2018-06-26 22:16:55 -0300

    add pestaña Servicios

--- mvc1/mvc1/Controllers/HomeController.cs
+++ mvc1/mvc1/Controllers/HomeController.cs
@@ -38,6 +38,11 @@ namespace mvc1.Controllers
             return View();
         }
 
+        public IActionResult Servicios() //probando
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
